ac31cd23c0eb2ccad64d646e6a7db86bd3c617d7
Alexandre Blache <alexandreblache.pro@gmail.com>
2023-01-05 12:31:44 +0100

    list

--- app/src/main/java/com/alex/planets/DetailsActivity.java
+++ app/src/main/java/com/alex/planets/DetailsActivity.java
@@ -31,15 +31,15 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         Intent intent = getIntent();
-        String test = intent.getStringExtra("planetName");
-        TextView textView = findViewById(R.id.planetNameDetails);
-        TextView textView3 = findViewById(R.id.textViewDetails3);
-        TextView textView4 = findViewById(R.id.subtitleDetails);
+        String intentPlanetNameExtra = intent.getStringExtra("planetName");
+        TextView planetNameView = findViewById(R.id.planetNameDetails);
+        TextView subtitleView = findViewById(R.id.subtitleDetails);
+        TextView aroundPlanetView = findViewById(R.id.aroundPlanetNameDetails);
         CircleImageView imageView = findViewById(R.id.detailsImage);
-        ListView detailsList = findViewById(R.id.detailsList);
+        ListView detailsMoonsList = findViewById(R.id.detailsList);
 
         PlanetDao planetDao = DatabaseClient.getInstance(getApplicationContext()).getAppDatabase().planetDao();
-        LiveData<Planet> planet = planetDao.getPlanet(test);
+        LiveData<Planet> planet = planetDao.getPlanet(intentPlanetNameExtra);
 
         planet.observe(this, new Observer<Planet>() {
             @SuppressLint("SetTextI18n")
@@ -49,7 +49,7 @@ public class DetailsActivity extends AppCompatActivity {
                 AroundPlanet aroundPlanet = planet.getAroundPlanet();
 
                 Glide.with(getApplicationContext()).load(planet.getImage()).into(imageView);
-                textView.setText(planet.getName());
+                planetNameView.setText(planet.getName());
 
                 if (moonList != null) {
                     List<String> moonNamesList = new ArrayList<>();
@@ -59,14 +59,14 @@ public class DetailsActivity extends AppCompatActivity {
                         moonNamesList.add(moon.getMoon());
                     }
 
-                    detailsList.setAdapter(adapter);
-                    textView3.setText("Satellites");
+                    detailsMoonsList.setAdapter(adapter);
+                    subtitleView.setText("Satellites");
                 }
 
                 if (aroundPlanet != null) {
                     String aroundPlanetName = aroundPlanet.getPlanet();
-                    textView3.setText("Tourne autour de");
-                    textView4.setText(aroundPlanetName.substring(0, 1).toUpperCase() + aroundPlanetName.substring(1));
+                    subtitleView.setText("Tourne autour de");
+                    aroundPlanetView.setText(aroundPlanetName.substring(0, 1).toUpperCase() + aroundPlanetName.substring(1));
                 }
             }
         });
